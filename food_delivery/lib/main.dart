26f3f709ebcd229fb64ea359876bbd13fb958525
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-03-06 00:02:16 -0300

    iniciando dependencias

--- food_delivery/lib/main.dart
+++ food_delivery/lib/main.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:food_delivery/pages/food/popular_food_detail.dart';
 import 'package:food_delivery/pages/food/recommended_food_detail.dart';
-import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
+import 'helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
